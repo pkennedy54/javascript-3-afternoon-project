0cfd14d5c14be93efbfaac69bb846a3bdc6ab0d6
pkennedy54 <pkennedy54v@gmail.com>
2020-06-15 16:46:34 -0600

    first commit

--- for-in-delete.js
+++ for-in-delete.js
@@ -11,13 +11,13 @@
   In the example below, we are accessing the property values. Uncomment the code below, run it and look at what prints in the console.
 */
 
-// var values = {
-//   one: 'These',
-//   two: ' are',
-//   three: ' the',
-//   four: ' property',
-//   five: ' values.'
-// } 
+var values = {
+   one: 'These',
+   two: ' are',
+   three: ' the',
+   four: ' property',
+   five: ' values.'
+ } 
 
 // for(var key in values) {
 //   console.log(values[key])
@@ -27,9 +27,8 @@
   In this next example, we are accessing the property names themselves. Uncomment the code below, run it and look at what prints in the console.
 */
 
-// for(var key in values) {
-//   console.log(key)
-// }
+for(var key in values) {
+   console.log(key) }
 
 
 
@@ -39,8 +38,9 @@
   Inside the function showValues, write a for in loop that concatenates each of the property values and returns the concatenated string.
 */
 
-function showValues( obj ) {
-  //Code Here
+function showValues( obj ) 
+  for (var prop in values)
+
 }
 
 
